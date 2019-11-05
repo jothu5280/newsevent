8c613ea0c3d3b78845abb938555065bf766fa7f2
johugh <johugh@gmx.com>
2019-11-05 10:48:15 -0700

    add search logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,11 +14,42 @@ class HomeController < ApplicationController
   	elsif !@output
   		@final_output = "Error"
   	else
-	  	@final_output = @output['articles'][0]['source']['id']
+	  	@final_output = @output['articles']
+	  	# @final_output = @output['articles'][0]['source']['id']
+
 	end
 
-	def topic
-	end
 
   end
+
+
+	def topic
+		@term_query = params[:topic]
+		if params[:topic] == ""
+			@term_query = "Please enter a topic to search for."
+		elsif params[:topic]
+			#do api stuff
+
+		  	require 'net/http'
+		  	require 'json'
+
+		  	@url = 'https://newsapi.org/v2/everything?domains=wsj.com?q=' + @term_query + '&apiKey=fde93151b2814cd68c8e0396dfda3e32'
+		  	@uri = URI(@url)
+		  	@response = Net::HTTP.get(@uri)
+		  	@output = JSON.parse(@response)
+		  	# @output = JSON.parse(@response)['articles'][0,5]
+
+		  	if @output.empty?
+		  		@final_output = "Error"
+		  	elsif !@output
+		  		@final_output = "Error"
+		  	else
+			  	@final_output = @output['articles']
+			end
+
+
+
+		end
+	end
+
 end
