e9338c1272adccdf9260533abb6981ca7a5914ad
johugh <johugh@gmx.com>
2019-11-05 10:49:56 -0700

    remove unneeded route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'home/index'
   root 'home#index'
 
-  get 'home/topic'
+  # get 'home/topic'
   post "topic" => 'home#topic'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
